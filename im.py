d53c29ea39ee152f6951a06eca3b190789f20d41
Haseen202 <faisalabdulaziz493@gmail.com>
2025-01-03 15:43:40 +0530

    Add files via upload

---
+++ im.py
@@ -0,0 +1,8 @@
+import calco3
+num1=10
+num2=5
+sum=calco3.add(num1,num2)
+print("sum = ",sum)
+print(f"sum = (sum)")
+print("Substraction:(calco3.substract){num1.num2}")
+print("Multiplication: (calco3.multiply){num1.num2}")
---
+++ output.py
@@ -0,0 +1,7 @@
+import calco3
+num1=10
+num2=5
+print (f"Addition : {calco3.add(num1,num2)}")
+print (f"Substraction : {calco3.substract(num1,num2)}")
+print (f"Multiplicationn : {calco3.multiply (num1,num2)}")
+print (f"Division : {calco3.divide(num1,num2)}")
